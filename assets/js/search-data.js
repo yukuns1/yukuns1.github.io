1800fcc4482abb091d9eddbffd7ccdbcbfdf40f6
yukuns1 <64042785+yukuns1@users.noreply.github.com>
2026-03-31 00:36:45 +0000

    Deploying to gh-pages from @ yukuns1/yukuns1.github.io@82e090482e152551c6c1d51b62d421ec3c297779 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=4BuUHg4AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
